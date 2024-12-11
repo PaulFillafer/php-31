7311cdc2738bc9dd300c0e25efa86dea60352d19
Paul Fillafer <pfillafer@tsn.at>
2024-12-11 23:07:57 +0100

    5. create.php

--- create.php
+++ create.php
@@ -1,3 +1,24 @@
+<?php
+
+require_once "models/Credentials.php";
+
+$c = new Credentials();
+
+if (empty($_POST)) {
+    $c->setName(isset($_POST['name']) ? $_POST['name'] : '');
+    $c->setDomain(isset($_POST['domain']) ? $_POST['domain'] : '');
+    $c->setCmsUsername(isset($_POST['cms_username']) ? $_POST['cms_username'] : '');
+    $c->setCmsPassword(isset($_POST['cms_password']) ? $_POST['cms_password'] : '');
+
+    if ($c->save()) {
+        header("Location: view.php?id=" . $c->getId());
+exit();
+    }
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="de">
 <head>
@@ -24,7 +45,15 @@
                 <div class="form-group required ">
                     <label class="control-label">Name *</label>
                     <input type="text" class="form-control" name="name" maxlength="32"
-                           value="">
+                           value="<?= $c->getName()?>">
+
+                    <?php
+                    if(!empty($c->getErrors()['name'])): ?>
+                    <div class="help-block"><?= $c->getName()['name']?></div>
+
+                    <?php
+                    endif;
+                    ?>
 
                 </div>
             </div>
@@ -33,7 +62,19 @@
                 <div class="form-group required ">
                     <label class="control-label">Domäne *</label>
                     <input type="text" class="form-control" name="domain" maxlength="128"
-                           value="">
+                           value="<?= $c->getDomain()?>">
+
+                    <?php
+                    if(!empty($c->getErrors()['domain'])): ?>
+                        <div class="help-block"><?= $c->getDomain()['domain']?></div>
+
+                    <?php
+                    endif;
+                    ?>
+
+
+
+
 
                 </div>
             </div>
@@ -44,7 +85,19 @@
                 <div class="form-group required ">
                     <label class="control-label">CMS-Benutzername *</label>
                     <input type="text" class="form-control" name="cms_username" maxlength="64"
-                           value="">
+                           value="<?= $c->getCmsUsername()?>">
+
+                    <?php
+                    if(!empty($c->getErrors()['cms_username'])): ?>
+                        <div class="help-block"><?= $c->getCmsUsername()['name']?></div>
+
+                    <?php
+                    endif;
+                    ?>
+
+
+
+
 
                 </div>
             </div>
@@ -53,7 +106,18 @@
                 <div class="form-group required ">
                     <label class="control-label">CMS-Passwort *</label>
                     <input type="text" class="form-control" name="cms_password" maxlength="64"
-                           value="">
+                           value="<?= $c->getCmsPassword()?>">
+
+                    <?php
+                    if(!empty($c->getErrors()['cms_password'])): ?>
+                        <div class="help-block"><?= $c->getCmsPassword()['cms_password']?></div>
+
+                    <?php
+                    endif;
+                    ?>
+
+
+
 
                 </div>
             </div>
